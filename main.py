b10fb64612f6505e146ad14744cbb658969b8bff
VladimirLevikov <v.v.levikov@gmail.com>
2025-06-09 18:23:35 +0300

    Добавлен вывод активных пропусков

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
+
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    active_passcards = Passcard.objects.filter(is_active=True)
+    all_passcards = Passcard.objects.all()
+
+    print(f'Количество активных пропусков: {active_passcards.count()}')
--- settings.py
+++ settings.py
@@ -1,11 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
